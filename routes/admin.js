216d533ddf29765b280f3630690b78e014a6bc30
christopher18-cyber <christopheradegoke4@gmail.com>
2025-10-01 02:50:25 +0100

    made more changes

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,10 @@ const Router = express.Router();
 const products = [];
 
 Router.get("/add-product", (req, res, next) => {
-  res.render("add-product", { pageTitle: "Add product" });
+  res.render("add-product", {
+    pageTitle: "Add product",
+    path: "/admin/add-product",
+  });
 });
 
 Router.post("/add-product", (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -6,6 +6,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const Router = express.Router();
 Router.get("/", (req, res, next) => {
-  res.render("shop", { prods: products, docTitle: "My love" });
+  res.render("shop", { prods: products, docTitle: "Shop", path: "/" });
 });
 export default Router;
